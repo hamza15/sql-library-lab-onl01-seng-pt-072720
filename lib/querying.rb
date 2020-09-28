55c18e98757d23eea3e1a161047d76df344783b8
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 06:07:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.author_id = characters.author_id
   GROUP BY characters.species
-  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
+  WHERE characters.species = 'human';"
 end
 
 def select_character_names_and_number_of_books_they_are_in
