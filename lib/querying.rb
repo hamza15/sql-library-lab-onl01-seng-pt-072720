59b0ffc4f374dbc825a4e9ef4496d9bf6e4c5d2e
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 06:01:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,8 +27,7 @@ def select_name_and_series_subgenres_of_authors
   JOIN series
   ON authors.id = series.id
   JOIN subgenres
-  ON 
-  "
+  ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
