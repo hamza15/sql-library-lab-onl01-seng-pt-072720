393e799e1f7f1b1c58d337a2a07456efc59d8cf1
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 05:33:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "Write your SQL query here"
   "SELECT books.title, books.year
   FROM books
-  JOIN series ON 
+  JOIN series 
+  ON books.id =  
   ORDER BY books.year"
 end
 
