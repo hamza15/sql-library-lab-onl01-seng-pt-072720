22813dc0299218a37572d5f6b1e64df37afcba3e
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 06:10:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,5 +41,7 @@ LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT("
+  "SELECT characters.name, COUNT(books.title)
+  FROM characters
+  "
 end
