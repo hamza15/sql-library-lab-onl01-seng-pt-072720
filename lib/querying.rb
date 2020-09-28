cd1e75c8a44746c912088f15a6df6c33879f9a4a
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 06:14:04 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,5 +48,5 @@ def select_character_names_and_number_of_books_they_are_in
   JOIN books
   ON character_books.book_id = books.id
   GROUP BY characters.name
-  ORDER_BY COUNT(books.title) ;"
+  ORDER_BY COUNT(books.title) DESC, characters.name;"
 end
