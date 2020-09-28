d66bc66b4082284a2114401ee7a7afb8ca818064
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 05:41:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,7 +15,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT()"
+  "SELECT characters.species, COUNT(characters.species)
+  FROM characters
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
