79c905b91fd08bdaa4ab36b2db49fdd9994adcab
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-09-28 06:03:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series
   JOIN characters
-  ON "
+  ON series.id = characters.id
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
